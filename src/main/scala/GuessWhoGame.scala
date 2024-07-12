c5133ef227fc375adea5be5c326d4667fc016701
Bilal Amiry <bezyy98@gmail.com>
2024-07-12 15:24:25 +0100

    Catch Up

--- src/main/scala/GuessWhoGame.scala
+++ src/main/scala/GuessWhoGame.scala
@@ -1,10 +1,17 @@
 import gameengine.GameEngine
 import characters.Resources
+import scala.util.Random
 
 object GuessWhoGame extends App{
   // Select random players of Characters
   val gameEngine: GameEngine = new GameEngine
   val guessWhoGame: Resources = new Resources
   println(gameEngine.selectRandomCharacter(guessWhoGame.returnListOfCharacters()))
+
+  val questions: Resources = new Resources
+
+
+
+  println(gameEngine.selectRandomQuestion(guessWhoGame.returnListOfQuestions()))
 
 }
--- src/main/scala/gameengine/GameEngine.scala
+++ src/main/scala/gameengine/GameEngine.scala
@@ -1,14 +1,22 @@
 package gameengine
-import characters.Person
+import characters.{Person, Resources}
 
 import scala.math.random
 import scala.util.Random
 class GameEngine {
+  val random = new Random()
 
   def selectRandomCharacter(characterList: List[Person]) : Person ={
-      val random = new Random()
+
       val reply = characterList(random.nextInt(characterList.length))
       reply
+  }
+
+
+  def selectRandomQuestion(questions: List[String]): String  = {
+
+    val questionair = questions(random.nextInt(questions.length))
+   questionair
   }
 
 
