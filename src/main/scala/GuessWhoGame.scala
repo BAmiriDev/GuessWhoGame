b02d6daf105a12875f2035f4167172a7e0d4ff14
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-12 15:23:57 +0100

    add method to generate random questions

--- src/main/scala/GuessWhoGame.scala
+++ src/main/scala/GuessWhoGame.scala
@@ -5,6 +5,8 @@ object GuessWhoGame extends App{
   // Select random players of Characters
   val gameEngine: GameEngine = new GameEngine
   val guessWhoGame: Resources = new Resources
+
   println(gameEngine.selectRandomCharacter(guessWhoGame.returnListOfCharacters()))
+  println(gameEngine.selectRandomQuestions(guessWhoGame.returnListOfQuestions()))
 
 }
--- src/main/scala/gameengine/GameEngine.scala
+++ src/main/scala/gameengine/GameEngine.scala
@@ -4,11 +4,14 @@ import characters.Person
 import scala.math.random
 import scala.util.Random
 class GameEngine {
-
+  val random = new Random()
   def selectRandomCharacter(characterList: List[Person]) : Person ={
-      val random = new Random()
       val reply = characterList(random.nextInt(characterList.length))
       reply
+  }
+  def selectRandomQuestions(questionsList: List[String]) : String ={
+    val question = questionsList(random.nextInt(questionsList.length))
+    question
   }
 
 
