0def8bd5efa21247b71b63b6ea8d9593338797de
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-12 13:34:51 +0100

    created person class

---
+++ src/main/scala/person/Person.scala
@@ -0,0 +1,18 @@
+package person
+
+class Person(
+              val gender: String,
+              val hairColour: String,
+              val hairLength: String,
+              val eyeColour: String,
+              val facialColour: String,
+              val accessories: String,
+              val complexion: String,
+              val clothingColor: String,
+              val clothingType: String,
+              val height: String
+
+            ) {
+
+
+}
