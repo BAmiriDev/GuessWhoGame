43854944fa5b34b2d5956dfbc75370fcea69a014
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-12 11:40:11 +0100

    removed unwanted changes

--- src/test/scala/GuessWhoGameSpec.java
+++ src/test/scala/GuessWhoGameSpec.java
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
+import org.scalatest.wordspec.AnyWordSpec;
 
 public class GuessWhoGameSpec {
 
-
-=======
-import org.scalatest.wordspec.AnyWordSpec;
-
-public class GuessWhoGameSpec extends AnyWordSpec {
->>>>>>> 59650e4e08679c5e54c50c1ce5065003d1366b10
 }
+
+
+
