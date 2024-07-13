498ca965a36e9b830deb1d49f546f7b63b93ec8c
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-13 11:14:47 +0100

    create gameBoard and Person

--- src/main/scala/GuessWhoGame.scala
+++ src/main/scala/GuessWhoGame.scala
@@ -1,22 +1,20 @@
-import gameengine.GameEngine
 import characters.Resources
-import scala.util.Random
+import gameengine.GameEngine
 
 object GuessWhoGame extends App{
   // Select random players of Characters
   val gameEngine: GameEngine = new GameEngine
   val guessWhoGame: Resources = new Resources
 
-  println(gameEngine.selectRandomCharacter(guessWhoGame.returnListOfCharacters()))
-  println(gameEngine.selectRandomQuestions(guessWhoGame.returnListOfQuestions()))
+  println("Create game and assign Board " + gameEngine.createPlayerAndAssignGameBoard() )
 
-  val questions: Resources = new Resources
-
-
+  println(gameEngine.selectRandomCharacter(guessWhoGame.charactersList))
+  println(gameEngine.selectRandomQuestions(guessWhoGame.listOfQuestions))
 
   val question = "Is the character male?"
   val answer = false
   println(gameEngine.returnListOfFilteredCharacters(question, answer))
+
 
 
 }
