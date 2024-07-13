70e1bb19fd557a435021a6dc7ffd9244d3d1a524
Bilal Amiry <bezyy98@gmail.com>
2024-07-13 11:27:54 +0100

    Fix Buffer

--- src/test/scala/GuessWhoGameSpec.scala
+++ src/test/scala/GuessWhoGameSpec.scala
@@ -8,15 +8,16 @@ class GuessWhoGameSpec extends AnyWordSpec {
   // Initialize game engine and resources once
   val gameEngine: GameEngine = new GameEngine
   val guessWhoGame: Resources = new Resources
+  val gameBoard: GameBoard = new GameBoard(Person)
 
   // Initialize expected values directly
   val expectedCharacters: List[Person] = guessWhoGame.charactersList
-  val expectedQuestions: List[String] = guessWhoGame.returnListOfQuestions
+  val expectedQuestions: List[String] = guessWhoGame.listOfQuestions
 
   "Resources.returnListOfCharacters" should {
     "return a list of characters" when {
       "called to retrieve the characters list" in {
-        val characters = guessWhoGame.returnListOfCharacters()
+        val characters = guessWhoGame.charactersList
         assert(characters == expectedCharacters)
       }
     }
@@ -25,7 +26,7 @@ class GuessWhoGameSpec extends AnyWordSpec {
   "Resources.returnListOfQuestions" should {
     "return a list of questions" when {
       "called to retrieve the questions list" in {
-        val questions = guessWhoGame.returnListOfQuestions()
+        val questions = guessWhoGame.listOfQuestions
         assert(questions == expectedQuestions)
       }
     }
@@ -34,7 +35,7 @@ class GuessWhoGameSpec extends AnyWordSpec {
   "GameEngine.selectRandomCharacter" should {
     "select a random character from the list" when {
       "called to select a random character" in {
-        val characters = guessWhoGame.returnListOfCharacters()
+        val characters = guessWhoGame.charactersList
         val selectedCharacter = gameEngine.selectRandomCharacter(characters)
         assert(characters.contains(selectedCharacter))
       }
@@ -44,7 +45,7 @@ class GuessWhoGameSpec extends AnyWordSpec {
   "GameEngine.selectRandomQuestion" should {
     "select a random question from the list" when {
       "called to select a random question" in {
-        val questions = guessWhoGame.returnListOfQuestions()
+        val questions = guessWhoGame.listOfQuestions
         val selectedQuestion = gameEngine.selectRandomQuestions(questions)
         assert(questions.contains(selectedQuestion))
       }
